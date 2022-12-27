2faa97e992c3d3f71be48344aa4e5dbc784172df
Luis Rosal <luiss.rosalg.13@gmail.com>
2022-12-27 17:05:19 -0400

    improved responsive design, matched colors and fonts with current design to maintain the brand's identity

--- src/headSection.tsx
+++ src/headSection.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export function HeadSection() {
   return (
-    <div className="bg-indigo-900 flow-root">
-      <div className='my-20 mx-20 flex-col text-center'>
-        <h1 className=" text-[4rem] italic font-bold text-white">About Deep Lawn</h1>
-        <p className='text-white m-4 text-[1.3rem]'>Deep Lawn helps lawn service companies do e-commerce like the tech companies.</p>
-        <button className='text-white font-bold text-lg bg-green-600 transition ease-in-out delay-800 hover:scale-105 hover:bg-green-400 duration-150 p-2 px-14 mt-8'>Sign Up</button>
+    <div className="bg-blue-600 flow-root">
+      <div className='my-12 mx-4 flex-col text-center'>
+        <h1 className="text-4xl font-bold text-white">About Deep Lawn</h1>
+        <p className='text-white m-4 text-lg'>Deep Lawn helps lawn service companies do e-commerce like the tech companies.</p>
+        <button className='text-white font-bold text-lg bg-green-600 transition ease-in-out delay-800 hover:scale-105 hover:bg-green-500 duration-150 p-2 px-14 mt-8'>Sign Up</button>
       </div>
     </div>
   );
--- src/infoSection.tsx
+++ src/infoSection.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function InfoSection() {
   return (
     <div className="flex mb-10 bg-gray-100">
-      <p className='py-8 px-[9rem] text-[1.3rem]' >
+      <p className='py-8 mx-6 md:mx-16 text-lg sm:text-xl text-center xl:mx-40 2xl:mx-60 lg:mx-28' >
       Deep Lawn is a platform for outdoor service companies to sell their services on their own website - in less than 60 seconds. We combine machine learning, geospatial data, payment processing, and CRM integrations into an all in one e-commerce tool to increase sales, close rates, and quote accuracy. 
       </p>
     </div>
